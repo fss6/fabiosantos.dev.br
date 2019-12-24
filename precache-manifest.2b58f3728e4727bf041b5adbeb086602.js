613117ce483d99af74f6b30f09b616d3a43d3a26
Fabio Santos <fabio@Fabios-MacBook-Pro.local>
2019-12-23 23:23:38 -0300

    Updates

---
+++ precache-manifest.2b58f3728e4727bf041b5adbeb086602.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dafd2a97c3107da4a250bb0990c6b5e8",
+    "url": "/fss6/fabiosantos.net.br/index.html"
+  },
+  {
+    "revision": "1b08c944ddae6b6fb0d9",
+    "url": "/fss6/fabiosantos.net.br/static/css/main.2eceba7e.chunk.css"
+  },
+  {
+    "revision": "9e91727bfc14f5b686bd",
+    "url": "/fss6/fabiosantos.net.br/static/js/2.3fd52a6c.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/fss6/fabiosantos.net.br/static/js/2.3fd52a6c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1b08c944ddae6b6fb0d9",
+    "url": "/fss6/fabiosantos.net.br/static/js/main.30b2057b.chunk.js"
+  },
+  {
+    "revision": "f2b845d60675b5b824be",
+    "url": "/fss6/fabiosantos.net.br/static/js/runtime-main.64216733.js"
+  }
+]);
