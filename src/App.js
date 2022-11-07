a9dae976a89332631acf3781f0c8a88dbc98a281
Fabio Santos <fabiosantos.pe@gmail.com>
2022-11-07 09:51:55 -0300

    remove unifbv link

--- src/App.js
+++ src/App.js
@@ -5,8 +5,6 @@ import {
 
 import Home from './component/Home'
 import Resume from './component/Resume'
-import Unifbv from './component/unifbv/Unifbv'
-import Period from './component/unifbv/Period'
 
 import './static/css/Main.css';
 import './static/css/Style.css';
@@ -17,10 +15,6 @@ function App() {
     <HashRouter>
       <Route path="/" exact={true} component={Home} />
       <Route path="/resume" exact={true} component={Resume} />
-      <Route path="/unifbv" exact={true} component={Unifbv} />
-      <Route path="/unifbv/:period" exact={true}>
-        <Child />
-      </Route>
     </ HashRouter>
   );
 }
